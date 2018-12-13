59be4828efdba131063713da4a98588dc04e87e6
Damian Magdaleno <44066467+SpackleDuck@users.noreply.github.com>
2018-12-13 09:51:12 -0800

    Some slider code
    incomplete slider code

--- Clothes_DesignerCircle_Function.py
+++ Clothes_DesignerCircle_Function.py
@@ -10,6 +10,17 @@ canvas.grid(row=0,rowspan=2, column=1)
 editor = tkinter.Text(root, width=10)
 editor.grid(column=2, row=0, rowspan=3)
 
+rotation_slider = tkinter.IntVar()
+rotation_slider.set(0)
+#class RotationSlider(tkinter.Scale):
+ #       def __init__(self, parent, myLabel, model_intvar, editor, canvas):
+  #          def slider_changed(newval):
+    #                tk_rotation = rotation_slider
+   #                 editor.insert(tkinter.END, tk_rotation+'\n')
+     #               editor.see(tkinter.END)
+      #              canvas.itemconfig(shapes[-1],fill=tk_rotation)
+       #             tkinter.Scale.__init__(self, parent, orient=tkinter.HORIZONTAL, from_=0, to=255,
+        #                        variable=model_intvar, label=myLabel, command=slider_changed)
 def down(event):
     global startx, starty
     startx = event.x 
