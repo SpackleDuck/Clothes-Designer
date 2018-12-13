dd1c88e188bb1c58fac53b42f12473c1a2ab10d7
cbadua <cj@chriselle.net>
2018-12-13 09:36:33 -0800

    Update Clothes_DesignerCircle_Function.py
    Updated with rectangle function

--- Clothes_DesignerCircle_Function.py
+++ Clothes_DesignerCircle_Function.py
@@ -5,6 +5,16 @@ root = tkinter.Tk()
 shapes = []
 canvas = tkinter.Canvas(root, width=300, height=300, background='#FFFFFF')
 canvas.grid(row=0,rowspan=2, column=1)
+option = tkinter.IntVar()
+
+shape =tkinter.Radiobutton(root, variable=option,
+                        text='click for circle', value=0)
+shape.grid(row=2,column=2)
+shape2 =tkinter.Radiobutton(root, variable=option,
+                    text='click for rectangle', value=1) 
+shape2.grid(row=1,column=2)
+option.set(0)
+
 def down(event):
     global startx, starty
     startx = event.x 
@@ -12,8 +22,12 @@ def down(event):
 
 def up(event):
     r = (startx-event.x)**2 + (starty-event.y)**2 
-    r = int(r**.5)                               
-    new_shape = canvas.create_oval(startx-r, starty-r, startx+r, starty+r,
+    r = int(r**.5)
+    if option.get() == 0: 
+        new_shape = canvas.create_oval(startx-r, starty-r, startx+r, starty+r,
+                                     outline='#000000')
+    elif option.get() == 1:
+        new_shape = canvas.create_rectangle(startx-r, starty-r, startx+r, starty+r,
                                      outline='#000000')
     shapes.append(new_shape)
 canvas.bind('<Button-1>', down)
